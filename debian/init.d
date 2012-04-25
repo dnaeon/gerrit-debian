a5bf52f2add6203bbd3063f50a511b68fa65fcfd
Marin Atanasov Nikolov <dnaeon@gmail.com>
2012-04-25 16:10:13 +0300

    Finish with the init.d script
    * Use a non-privileged system user
    * Remove un-needed daemon dependency
    * Fix location to the review_site/gerrit.sh script

--- debian/init.d
+++ debian/init.d
@@ -13,22 +13,16 @@
 # PATH should only include /usr/* if it runs after the mountnfs.sh script
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
 
-DESC=gerrit
-NAME=gerrit
+DESC="Gerrit Code Review"
+NAME="gerrit"
 SCRIPTNAME=/etc/init.d/$NAME
 
 # Read configuration variable file if it is present
 [ -r /etc/default/$NAME ] && . /etc/default/$NAME
 
-DAEMON=/usr/bin/daemon
-DAEMON_ARGS=""
-
 PIDFILE=${GERRIT_SITE}/logs/$NAME.pid
 
 SU=/bin/su
-
-# Exit if the package is not installed
-[ -x $DAEMON ] || exit 0
 
 # Load the VERBOSE setting and other rcS variables
 . /lib/init/vars.sh
@@ -42,12 +36,12 @@ SU=/bin/su
 # 
 gerrit_initialize()
 {
-    echo "No Gerrit site found. Will Initialize Gerrit first..."
+    /bin/echo -e "\nNo Gerrit site found. Will Initialize Gerrit first..."
 
-    [ ! -d "${GERRIT_DATADIR}" ] && install -d -o ${GERRIT_USER} -g ${GERRIT_GROUP} -m 750 ${GERRIT_DATADIR}
-    
-    [ ! -d "${GERRIT_SITE}" ] && \
-	${SU} -l ${GERRIT_USER} --shell=/bin/sh -c "${DAEMON} ${DAEMON_ARGS} -- ${JAVA} ${JAVA_ARGS} -jar ${GERRIT_WAR} ${GERRIT_ARGS} init -d ${GERRIT_SITE}"
+    if [ ! -d "${GERRIT_SITE}" ]; then
+	install -d -o ${GERRIT_USER} -g ${GERRIT_GROUP} -m 0750 ${GERRIT_DATADIR}
+	${SU} -l ${GERRIT_USER} --shell=/bin/sh -c "${JAVA} ${JAVA_ARGS} -jar ${GERRIT_WAR} ${GERRIT_ARGS} init -d ${GERRIT_SITE}"
+    fi
 }
 
 #
@@ -60,12 +54,14 @@ do_start()
 	#   1 if daemon was already running
 	#   2 if daemon could not be started
     
-	${DAEMON} ${DAEMON_ARGS} --running && return 1
-	
+    if get_daemon_status; then return 1; fi
+    
 	# Initialize Gerrit if it is not yet initialized
-	[ ! -d "${GERRIT_DATADIR}" -o ! -d "${GERRIT_SITE}" ] && gerrit_initialize
-	
-	${GERRIT_SH} start
+    [ ! -d "${GERRIT_SITE}" ] && gerrit_initialize
+    
+    ${GERRIT_SH} start > /dev/null 2>&1
+
+    return $?
 }
 
 #
@@ -81,14 +77,19 @@ force_stop()
 {
     get_running
     if [ $? -ne 0 ]; then
-        killall -u $GERRIT_USER java daemon || return 3
+        killall -u ${GERRIT_USER} java || return 3
     fi
 }
 
 # Get the status of the daemon process
 get_daemon_status()
 {
-    $DAEMON $DAEMON_ARGS --running || return 1
+    if [ -f ${PIDFILE} ]; then
+	rc_pid=`cat ${PIDFILE}`
+	procname=`ps -o ucomm= ${rc_pid}`
+    fi
+
+    [ -z ${procname} ] && return 1 || return 0
 }
 
 #
@@ -102,69 +103,69 @@ do_stop()
 	#   2 if daemon could not be stopped
 	#   other if a failure occurred
 
-    	get_daemon_status
-	case "$?" in
-            0)
-		$DAEMON $DAEMON_ARGS --stop || return 2
+    get_daemon_status
+    case "$?" in
+        0)
+	    ${GERRIT_SH} stop > /dev/null 2>&1
         	# wait for the process to really terminate
-		for n in 1 2 3 4 5; do
-		    sleep 1
-		    $DAEMON $DAEMON_ARGS --running || break
-		done
-		if get_daemon_status; then
-		    force_stop || return 3
-		fi
-		;;
-            *)
+	    for n in 1 2 3 4 5; do
+		sleep 1
+		get_daemon_status || break
+	    done
+	    if get_daemon_status; then
 		force_stop || return 3
-		;;
-	esac
-	
+	    fi
+	    ;;
+        *)
+	    force_stop || return 3
+	    ;;
+    esac
+    
 	# Many daemons don't delete their pidfiles when they exit.
-	rm -f $PIDFILE
-	return 0
+    rm -f $PIDFILE
+    return 0
 }
 
 case "$1" in
-  start)
-    log_daemon_msg "Starting $DESC " "$NAME"
-    do_start
-    case "$?" in
-		0|1) log_end_msg 0 ;;
-		2) log_end_msg 1 ;;
+    start)
+	log_daemon_msg "Starting $DESC " "$NAME"
+	do_start
+	case "$?" in
+	    0|1) log_end_msg 0 ;;
+	    2) log_end_msg 2 ;;
 	esac
-  ;;
-  stop)
+	;;
+    stop)
 	log_daemon_msg "Stopping $DESC" "$NAME"
 	do_stop
 	case "$?" in
-		0|1) log_end_msg 0 ;;
-		2) log_end_msg 1 ;;
+	    0|1) log_end_msg 0 ;;
+	    2) log_end_msg 1 ;;
 	esac
 	;;
-  status)
-       get_daemon_status
-       case "$?" in
-           0) echo "$DESC is running with the pid `cat $PIDFILE`";;
-           *)
-               get_running
-               procs=$?
-               if [ $procs -eq 0 ]; then
-                   echo -n "$DESC is not running"
-                   if [ -f $PIDFILE ]; then
-                       echo ", but the pidfile ($PIDFILE) still exists"
-                   else
-                       echo
-                   fi
+    status)
+	get_daemon_status
+	case "$?" in
+            0) echo "$DESC is running with the pid `cat $PIDFILE`";;
+            *)
+		get_running
+		procs=$?
+		if [ $procs -eq 0 ]; then
+                    echo -n "$DESC is not running"
+                    if [ -f $PIDFILE ]; then
+			echo ", but the pidfile ($PIDFILE) still exists"
+                    else
+			echo
+                    fi
 
-               else
-                   echo "$procs instances of jenkins are running at the moment"
-                   echo "but the pidfile $PIDFILE is missing"
-               fi
-               ;;
-       esac
-       ;;
-  restart|force-reload)
+		else
+                    echo "$procs instances of Gerrit are running at the moment"
+                    echo "but the pidfile $PIDFILE is missing"
+		fi
+		;;
+	esac
+	;;
+    restart|force-reload)
 	#
 	# If the "reload" option is implemented then remove the
 	# 'force-reload' alias
@@ -172,21 +173,21 @@ case "$1" in
 	log_daemon_msg "Restarting $DESC" "$NAME"
 	do_stop
 	case "$?" in
-	  0|1)
+	    0|1)
 		do_start
 		case "$?" in
-			0) log_end_msg 0 ;;
-			1) log_end_msg 1 ;; # Old process is still running
-			*) log_end_msg 1 ;; # Failed to start
+		    0) log_end_msg 0 ;;
+		    1) log_end_msg 1 ;; # Old process is still running
+		    *) log_end_msg 1 ;; # Failed to start
 		esac
 		;;
-	  *)
+	    *)
 	  	# Failed to stop
-		log_end_msg 1
+		log_failure_msg; log_end_msg 1
 		;;
 	esac
 	;;
-  *)
+    *)
 	echo "Usage: $SCRIPTNAME {start|stop|status|restart|force-reload}" >&2
 	exit 3
 	;;
