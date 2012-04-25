902673ceb91555802258ce61cc8ad877d227ef38
Marin Atanasov Nikolov <dnaeon@gmail.com>
2012-04-25 14:02:55 +0300

    Prepare init.d script for Gerrit
    * Parts borrowed from the Jenkins init.d script

--- debian/init.d.ex
+++ debian/init.d.ex
@@ -5,28 +5,30 @@
 # Required-Stop:
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: <Enter a short description of the sortware>
-# Description:       <Enter a long description of the software>
-#                    <...>
-#                    <...>
+# Description:       Web based code review and project management for Git based projects
 ### END INIT INFO
 
-# Author: mra <mra@mra-debian-vbox.sofia.elex.be>
+# Author: Marin Atanasov Nikolov <mra@melexis.com>
 
 # PATH should only include /usr/* if it runs after the mountnfs.sh script
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
-DESC=gerrit             # Introduce a short description here
-NAME=gerrit             # Introduce the short server's name here
-DAEMON=/usr/sbin/gerrit # Introduce the server's location here
-DAEMON_ARGS=""             # Arguments to run the daemon with
-PIDFILE=/var/run/$NAME.pid
-SCRIPTNAME=/etc/init.d/$NAME
 
-# Exit if the package is not installed
-[ -x $DAEMON ] || exit 0
+DESC=gerrit
+NAME=gerrit
+SCRIPTNAME=/etc/init.d/$NAME
 
 # Read configuration variable file if it is present
 [ -r /etc/default/$NAME ] && . /etc/default/$NAME
+
+DAEMON=/usr/bin/daemon
+DAEMON_ARGS=""
+
+PIDFILE=${GERRIT_SITE}/logs/$NAME.pid
+
+SU=/bin/su
+
+# Exit if the package is not installed
+[ -x $DAEMON ] || exit 0
 
 # Load the VERBOSE setting and other rcS variables
 . /lib/init/vars.sh
@@ -34,6 +36,19 @@ SCRIPTNAME=/etc/init.d/$NAME
 # Define LSB log_* functions.
 # Depend on lsb-base (>= 3.0-6) to ensure that this file is present.
 . /lib/lsb/init-functions
+
+#
+# Function that initializes Gerrit
+# 
+gerrit_initialize()
+{
+    echo "No Gerrit site found. Will Initialize Gerrit first..."
+
+    [ ! -d "${GERRIT_DATADIR}" ] && install -d -o ${GERRIT_USER} -g ${GERRIT_GROUP} -m 750 ${GERRIT_DATADIR}
+    
+    [ ! -d "${GERRIT_SITE}" ] && \
+	${SU} -l ${GERRIT_USER} --shell=/bin/sh -c "${DAEMON} ${DAEMON_ARGS} -- ${JAVA} ${JAVA_ARGS} -jar ${GERRIT_WAR} ${GERRIT_ARGS} init -d ${GERRIT_SITE}"
+}
 
 #
 # Function that starts the daemon/service
@@ -44,14 +59,36 @@ do_start()
 	#   0 if daemon has been started
 	#   1 if daemon was already running
 	#   2 if daemon could not be started
-	start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON --test > /dev/null \
-		|| return 1
-	start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON -- \
-		$DAEMON_ARGS \
-		|| return 2
-	# Add code here, if necessary, that waits for the process to be ready
-	# to handle requests from services started subsequently which depend
-	# on this one.  As a last resort, sleep for some time.
+    
+	${DAEMON} ${DAEMON_ARGS} --running && return 1
+	
+	# Initialize Gerrit if it is not yet initialized
+	[ ! -d "${GERRIT_DATADIR}" -o ! -d "${GERRIT_SITE}" ] && gerrit_initialize
+	
+	${GERRIT_SH} start
+}
+
+#
+# Verify that all Gerrit processes have been shutdown
+# and if not, then do killall for them
+# 
+get_running() 
+{
+    return `ps -U $GERRIT_USER --no-headers -f | egrep -e '(java|daemon)' | grep -c . `
+}
+
+force_stop() 
+{
+    get_running
+    if [ $? -ne 0 ]; then
+        killall -u $GERRIT_USER java daemon || return 3
+    fi
+}
+
+# Get the status of the daemon process
+get_daemon_status()
+{
+    $DAEMON $DAEMON_ARGS --running || return 1
 }
 
 #
@@ -64,64 +101,69 @@ do_stop()
 	#   1 if daemon was already stopped
 	#   2 if daemon could not be stopped
 	#   other if a failure occurred
-	start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --pidfile $PIDFILE --name $NAME
-	RETVAL="$?"
-	[ "$RETVAL" = 2 ] && return 2
-	# Wait for children to finish too if this is a daemon that forks
-	# and if the daemon is only ever run from this initscript.
-	# If the above conditions are not satisfied then add some other code
-	# that waits for the process to drop all resources that could be
-	# needed by services started subsequently.  A last resort is to
-	# sleep for some time.
-	start-stop-daemon --stop --quiet --oknodo --retry=0/30/KILL/5 --exec $DAEMON
-	[ "$?" = 2 ] && return 2
+
+    	get_daemon_status
+	case "$?" in
+            0)
+		$DAEMON $DAEMON_ARGS --stop || return 2
+        	# wait for the process to really terminate
+		for n in 1 2 3 4 5; do
+		    sleep 1
+		    $DAEMON $DAEMON_ARGS --running || break
+		done
+		if get_daemon_status; then
+		    force_stop || return 3
+		fi
+		;;
+            *)
+		force_stop || return 3
+		;;
+	esac
+	
 	# Many daemons don't delete their pidfiles when they exit.
 	rm -f $PIDFILE
-	return "$RETVAL"
-}
-
-#
-# Function that sends a SIGHUP to the daemon/service
-#
-do_reload() {
-	#
-	# If the daemon can reload its configuration without
-	# restarting (for example, when it is sent a SIGHUP),
-	# then implement that here.
-	#
-	start-stop-daemon --stop --signal 1 --quiet --pidfile $PIDFILE --name $NAME
 	return 0
 }
 
 case "$1" in
   start)
-    [ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC " "$NAME"
+    log_daemon_msg "Starting $DESC " "$NAME"
     do_start
     case "$?" in
-		0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-		2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
+		0|1) log_end_msg 0 ;;
+		2) log_end_msg 1 ;;
 	esac
   ;;
   stop)
-	[ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
+	log_daemon_msg "Stopping $DESC" "$NAME"
 	do_stop
 	case "$?" in
-		0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
-		2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
+		0|1) log_end_msg 0 ;;
+		2) log_end_msg 1 ;;
 	esac
 	;;
   status)
-       status_of_proc "$DAEMON" "$NAME" && exit 0 || exit $?
+       get_daemon_status
+       case "$?" in
+           0) echo "$DESC is running with the pid `cat $PIDFILE`";;
+           *)
+               get_running
+               procs=$?
+               if [ $procs -eq 0 ]; then
+                   echo -n "$DESC is not running"
+                   if [ -f $PIDFILE ]; then
+                       echo ", but the pidfile ($PIDFILE) still exists"
+                   else
+                       echo
+                   fi
+
+               else
+                   echo "$procs instances of jenkins are running at the moment"
+                   echo "but the pidfile $PIDFILE is missing"
+               fi
+               ;;
+       esac
        ;;
-  #reload|force-reload)
-	#
-	# If do_reload() is not implemented then leave this commented out
-	# and leave 'force-reload' as an alias for 'restart'.
-	#
-	#log_daemon_msg "Reloading $DESC" "$NAME"
-	#do_reload
-	#log_end_msg $?
-	#;;
   restart|force-reload)
 	#
 	# If the "reload" option is implemented then remove the
@@ -145,10 +187,9 @@ case "$1" in
 	esac
 	;;
   *)
-	#echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload}" >&2
 	echo "Usage: $SCRIPTNAME {start|stop|status|restart|force-reload}" >&2
 	exit 3
 	;;
 esac
 
-:
+exit 0
